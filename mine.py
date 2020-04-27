b31b4b9247437b4923d1502257b72345b0705d2a
liamhellend <liamhellend@gmail.com>
2020-04-27 22:03:27 +0800

    added: mine.py

---
+++ mine.py
@@ -0,0 +1,101 @@
+import os
+from pathlib import Path
+import json
+import hashlib
+import datetime
+import base64
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+class FileModifiedHandler(FileSystemEventHandler):
+
+    def __init__(self, path, file_name, callback):
+        self.file_name = file_name
+        self.callback = callback
+
+        # set observer to watch for changes in the directory
+        self.observer = Observer()
+        self.observer.schedule(self, path, recursive=False)
+        self.observer.start()
+        self.observer.join()
+
+    def on_modified(self, event): 
+        # only act on the change that we're looking for
+        if not event.is_directory and event.src_path.endswith(self.file_name):
+            self.observer.stop() # stop watching
+            self.callback() # call callback
+
+
+def createGenisisBlock():
+    file = Path("blockchain.json")
+    if (file.is_file() != True or os.stat("blockchain.json").st_size == 0):
+        with open("blockchain.json", "w") as file:
+
+            block_data = "first block"
+
+            block = {
+            "index": 0,
+            "data": block_data,
+            "Timestamp": datetime.datetime.now().isoformat(),
+            "previousHash": 0,
+            "hash": hashlib.sha256(block_data.encode('utf-8')).hexdigest()
+            }
+
+            json.dump(block, file)
+            file.write("\n")
+            file.close()
+            return
+    else:
+        return
+    
+
+def createRegularBlock():
+
+    with open("transactions.json", "r") as transac_file:
+        transaction = transac_file.readlines()[-1]
+        transac_file.close()
+    
+    with open("blockchain.json", "r") as block_file:
+        previousBlock = block_file.readlines()[-1]
+        previousBlock = json.loads(previousBlock)
+        print(previousBlock)
+        block_file.close()
+
+
+    with open("blockchain.json", "a") as block_file:
+
+        block = {
+            "Index": 0,
+            "Data": transaction,
+            "Timestamp": datetime.datetime.now().isoformat(),
+            "PreviousHash": previousBlock["previousHash"]
+            }
+        
+        nonce = 0
+        while True:
+
+            block = json.dumps(block)
+            
+            block_hash = hashlib.sha256(block.encode('utf-8') + str(nonce).encode('utf-8')).hexdigest()
+
+            print(block_hash)
+
+            if(block_hash[:4] == "0"*4):
+                print("found nonce")
+                break
+            nonce += 1
+
+
+        json.dump(block, block_file)
+        block_file.write("\n")
+        block_file.close()
+
+def callback():
+    createRegularBlock()
+
+
+createGenisisBlock()
+
+while True:
+    FileModifiedHandler('.', "transactions.json", callback)
